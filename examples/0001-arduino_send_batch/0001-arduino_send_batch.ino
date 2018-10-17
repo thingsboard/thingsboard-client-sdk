9f332b1839da86f0b266a99914c5c29c72dfddd4
Max Payne <forgge@gmail.com>
2018-10-17 17:29:01 +0300

    Improve examples and add travis badge
    New native ESP example is added. Sorting of examples overall improved.

--- examples/0001-arduino_send_batch/0001-arduino_send_batch.ino
+++ examples/0001-arduino_send_batch/0001-arduino_send_batch.ino
@@ -1,3 +1,10 @@
+// This sketch demonstrates connecting and sending telemetry and attributes
+// it batch using ThingsBoard SDK
+//
+// Hardware:
+//  - Arduino Uno
+//  - ESP8266 connected to Arduino Uno
+
 #include "ThingsBoard.h"
 
 #include <WiFiEspClient.h>
