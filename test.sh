5c262bd299bebe8b5ee875822b48a43951df41eb
Max Payne <forgge@gmail.com>
2018-12-29 01:22:44 +0200

    Arduino ThingsBoard HTTP class and test script improvement

--- test.sh
+++ test.sh
@@ -1,9 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+EXAMPLES_ARDUINO_UNO=(
+    "examples/0000-arduino_send_telemetry"
+    "examples/0001-arduino_send_batch"
+    "examples/0002-arduino_rpc"
+    "examples/0004-arduino-sim900_send_telemetry"
+)
+
+EXAMPLES_ESP8266=(
+    "examples/0003-esp8266_send_data"
+)
+
+EXAMPLES=( "${EXAMPLES_ESP8266[@]}" "${EXAMPLES_ARDUINO_UNO[@]}")
+
 # Test if arduino command line interface is downloaded locally
-if [ -f "$(pwd)"/arduino-cli-0.3.2-alpha.preview-linux64 ]
+if [ -f "$(pwd)/arduino-cli-0.3.2-alpha.preview-linux64" ]
 then
     ARDUINO_CLI="$(pwd)/arduino-cli-0.3.2-alpha.preview-linux64"
     echo "Found arduino CLI in $ARDUINO_CLI"
@@ -12,29 +25,37 @@ else
 fi
 
 do_test() {
-    $ARDUINO_CLI compile -b arduino:avr:uno examples/0000-arduino_send_telemetry/
-    $ARDUINO_CLI compile -b arduino:avr:uno examples/0001-arduino_send_batch/
-    $ARDUINO_CLI compile -b arduino:avr:uno examples/0002-arduino_rpc/
+    for path in "${EXAMPLES_ARDUINO_UNO[@]}"
+    do
+        "${ARDUINO_CLI}" compile --warnings all -v -b arduino:avr:uno "${path}"
+    done
+
+    for path in "${EXAMPLES_ESP8266[@]}"
+    do
+        "${ARDUINO_CLI}" compile -b esp8266:esp8266:generic "${path}"
+    done
 }
 
 # Create symlinks to library under test
+do_link() {
+    for path in "${EXAMPLES[@]}"
+    do
+        ln -sf "$(pwd)/src/ThingsBoard.h" "${path}/ThingsBoard.h"
+        ln -sf "$(pwd)/src/ThingsBoard.cpp" "${path}/ThingsBoard.cpp"
+    done
+}
 
-ln -sf "$(pwd)/src/ThingsBoard.h" "examples/0000-arduino_send_telemetry/ThingsBoard.h"
-ln -sf "$(pwd)/src/ThingsBoard.h" "examples/0001-arduino_send_batch/ThingsBoard.h"
-ln -sf "$(pwd)/src/ThingsBoard.h" "examples/0002-arduino_rpc/ThingsBoard.h"
-
-ln -sf "$(pwd)/src/ThingsBoard.cpp" "examples/0000-arduino_send_telemetry/ThingsBoard.cpp"
-ln -sf "$(pwd)/src/ThingsBoard.cpp" "examples/0001-arduino_send_batch/ThingsBoard.cpp"
-ln -sf "$(pwd)/src/ThingsBoard.cpp" "examples/0002-arduino_rpc/ThingsBoard.cpp"
+do_link
 
 # Dependencies
 
-# Ignore if already installed
-$ARDUINO_CLI lib install PubSubClient || true
-# Ignore if already installed
-$ARDUINO_CLI lib install WiFiEsp || true
-# Ignore if already installed
-$ARDUINO_CLI lib install ArduinoJson || true
+# `|| true`` added to ignore an error if a lib is already installed
+
+"${ARDUINO_CLI}" lib install PubSubClient || true
+"${ARDUINO_CLI}" lib install WiFiEsp || true
+"${ARDUINO_CLI}" lib install ArduinoJson || true
+"${ARDUINO_CLI}" lib install TinyGSM || true
+"${ARDUINO_CLI}" lib install ArduinoHttpClient || true
 
 do_test
 
