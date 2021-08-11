28c9d0c9882c59b87b02e8b0ac153f11398faec7
Justin K <r0g-dev@outlook.com>
2021-08-11 08:57:34 -0500

    Deserialize RPC params

--- src/ThingsBoard.h
+++ src/ThingsBoard.h
@@ -292,6 +292,7 @@ private:
       const JsonObject &data = jsonBuffer.template as<JsonObject>();
 
       const char *methodName = data["method"];
+      const char *params = data["params"];
 
       if (methodName) {
         Logger::log("received RPC:");
@@ -310,10 +311,23 @@ private:
           // Do not inform client, if parameter field is missing for some reason
           if (!data.containsKey("params")) {
             Logger::log("no parameters passed with RPC, passing null JSON");
+          } else {
+            Logger::log("params:");
+            Logger::log(params);
           }
+
+          // deserialize params
+          StaticJsonDocument<JSON_OBJECT_SIZE(MaxFieldsAmt)> doc;
+          DeserializationError err_param = deserializeJson(doc, params);
+          if (err_param) {
+            Logger::log("unable to de-serialize params: ");
+            Logger::log(err_param.f_str());
+          }
+          const JsonObject &param = doc.template as<JsonObject>();
+
           // Getting non-existing field from JSON should automatically
           // set JSONVariant to null
-          r = m_rpcCallbacks[i].m_cb(data["params"]);
+          r = m_rpcCallbacks[i].m_cb(param);
           break;
         }
       }
