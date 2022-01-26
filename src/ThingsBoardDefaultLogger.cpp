11ca71af8dc157fbb0670babc4d0e98256441dcb
OekoSolveMG <matteo.gmuer@oekosolve.ch>
2022-01-26 09:02:03 +0100

    Add unlimited RPC and Shared Attribute callbacks.

--- src/ThingsBoardDefaultLogger.cpp
+++ src/ThingsBoardDefaultLogger.cpp
@@ -1,6 +1,9 @@
 // Header include.
 #include "ThingsBoardDefaultLogger.h"
 
+// Library include.
+#include "HardwareSerial.h"
+
 void ThingsBoardDefaultLogger::log(const char *msg) {
   Serial.print(F("[TB] "));
   Serial.println(msg);
