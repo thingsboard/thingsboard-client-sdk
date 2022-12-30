5521ae0210d91d6ebe473b9b7689ccf2e81c949c
MathewHDYT <48954742+MathewHDYT@users.noreply.github.com>
2022-12-30 20:20:27 +0100

    Improved OTA / provision example for ESP32.

--- examples/0009-esp8266_esp32_process_OTA_MQTT/0009-esp8266_esp32_process_OTA_MQTT.ino
+++ examples/0009-esp8266_esp32_process_OTA_MQTT/0009-esp8266_esp32_process_OTA_MQTT.ino
@@ -4,60 +4,78 @@
 #include <WiFi.h>
 #endif
 
-#include "ThingsBoard.h"
+#include <ThingsBoard.h>
 
-#define CURRENT_FIRMWARE_TITLE    "TEST"
-#define CURRENT_FIRMWARE_VERSION  "1.0.0"
 
-#define WIFI_SSID           "YOUR_WIFI_SSID"
-#define WIFI_PASSWORD       "YOUR_WIFI_PASSWORD"
+// Firmware title and version used to compare with remote version, to check if an update is needed.
+// Title needs to be the same and version needs to be different --> downgrading is possible
+constexpr char CURRENT_FIRMWARE_TITLE[] PROGMEM = "TEST";
+constexpr char CURRENT_FIRMWARE_VERSION[] PROGMEM = "1.0.0";
+
+// Maximum amount of retries we attempt to download each firmware chunck over MQTT
+constexpr uint8_t FIRMWARE_FAILURE_RETRIES PROGMEM = 5U;
+// Size of each firmware chunck downloaded over MQTT,
+// increased packet size, might increase download speed
+constexpr uint16_t FIRMWARE_PACKET_SIZE PROGMEM = 4096U;
+
+constexpr char WIFI_SSID[] PROGMEM = "YOUR_WIFI_SSID";
+constexpr char WIFI_PASSWORD[] PROGMEM = "YOUR_WIFI_PASSWORD";
 
 // See https://thingsboard.io/docs/getting-started-guides/helloworld/
 // to understand how to obtain an access token
-#define TOKEN               "YOUR_DEVICE_ACCESS_TOKEN"
-#define THINGSBOARD_SERVER  "thingsboard.cloud"
+constexpr char TOKEN[] PROGMEM = "YOUR_DEVICE_ACCESS_TOKEN";
+
+// Thingsboard we want to establish a connection too
+constexpr char THINGSBOARD_SERVER[] PROGMEM = "demo.thingsboard.io";
+// MQTT port used to communicate with the server, 1883 is the default unencrypted MQTT port,
+// whereas 8883 would be the default encrypted SSL MQTT port
+constexpr uint16_t THINGSBOARD_PORT PROGMEM = 1883;
+
+// Maximum size packets will ever be sent or received by the underlying MQTT client,
+// if the size is to small messages might not be sent or received messages will be discarded
+constexpr uint32_t MQTT_MAX_MESSAGE_SIZE PROGMEM = FIRMWARE_PACKET_SIZE + 512U;
+
+// Baud rate for the debugging serial connection
+constexpr uint32_t SERIAL_DEBUG_BAUD PROGMEM = 115200U;
 
 
-// Baud rate for debug serial
-#define SERIAL_DEBUG_BAUD   115200
-
-// Initialize ThingsBoard client
+// Initialize underlying client, used to establish a connection
 WiFiClient espClient;
-// Initialize ThingsBoard instance
-//ThingsBoard tb(espClient);
-ThingsBoardSized<512> tb(espClient); //increase buffer size
+// Initialize ThingsBoard instance with the maximum needed buffer size
+ThingsBoardSized<MQTT_MAX_MESSAGE_SIZE> tb(espClient);
 
-// the Wifi radio's status
-int status = WL_IDLE_STATUS;
 
-void InitWiFi()
-{
+/// @brief Initalizes WiFi connection,
+// will endlessly delay until a connection has been successfully established
+void InitWiFi() {
   Serial.println("Connecting to AP ...");
-  // attempt to connect to WiFi network
-
+  // Attempting to establish a connection to the given WiFi network
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
-
+    // Delay 500ms until a connection has been succesfully established
     delay(500);
     Serial.print(".");
   }
   Serial.println("Connected to AP");
 }
 
-void reconnect() {
-  // Loop until we're reconnected
-  status = WiFi.status();
-  if ( status != WL_CONNECTED) {
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    Serial.println("Connected to AP");
+/// @brief Reconnects the WiFi uses InitWiFi if the connection has been removed
+/// @return Returns true as soon as a connection has been established again
+const bool reconnect() {
+  // Check to ensure we aren't connected yet
+  const wl_status_t status = WiFi.status();
+  if (status == WL_CONNECTED) {
+    return true;
   }
+
+  // If we aren't establish a new connection to the given WiFi network
+  InitWiFi();
+  return true;
 }
 
-void UpdatedCallback(const bool& success) {
+/// @brief Updated callback that will be called as soon as the firmware update finishes
+/// @param success Either true (update succesfull) or false (update failed)
+void updatedCallback(const bool& success) {
   if (success) {
     Serial.println("Done, Reboot now");
 #if defined(ESP8266)
@@ -65,39 +83,38 @@ void UpdatedCallback(const bool& success) {
 #elif defined(ESP32)
     esp_restart();
 #endif
+    return;
   }
-  else {
-    Serial.println("No new firmware");
-  }
+  Serial.println("Downloading firmware failed");
 }
 
 void setup() {
-  // initialize serial for debugging
+  // Initalize serial connection for debugging
   Serial.begin(SERIAL_DEBUG_BAUD);
-  Serial.println();
+  delay(1000);
   InitWiFi();
 }
 
 void loop() {
   delay(1000);
 
-  if (WiFi.status() != WL_CONNECTED) {
-    reconnect();
+  if (!reconnect()) {
+    return;
   }
 
   if (!tb.connected()) {
-    // Connect to the ThingsBoard
-    Serial.print("Connecting to: ");
-    Serial.print(THINGSBOARD_SERVER);
-    Serial.print(" with token ");
-    Serial.println(TOKEN);
-    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+    // Reconnect to the ThingsBoard server,
+    // if a connection was disrupted or has not yet been established
+    Serial.printf("Connecting to: (%s) with token (%s)\n", THINGSBOARD_SERVER, TOKEN);
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN, THINGSBOARD_PORT)) {
       Serial.println("Failed to connect");
       return;
     }
 
     Serial.println("Firwmare Update...");
-    tb.Start_Firmware_Update(CURRENT_FIRMWARE_TITLE, CURRENT_FIRMWARE_VERSION, UpdatedCallback);
+    // See https://thingsboard.io/docs/user-guide/ota-updates/
+    // to understand how to create a new OTA pacakge and assign it to a device so it can download it.
+    tb.Start_Firmware_Update(CURRENT_FIRMWARE_TITLE, CURRENT_FIRMWARE_VERSION, &updatedCallback, FIRMWARE_FAILURE_RETRIES, FIRMWARE_PACKET_SIZE);
   }
 
   tb.loop();
