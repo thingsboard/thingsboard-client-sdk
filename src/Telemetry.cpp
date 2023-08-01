9e4e1a798a57da2d17cd2814190396c2fccbe009
MathewHDYT <48954742+MathewHDYT@users.noreply.github.com>
2023-08-01 18:29:08 +0200

    Revert removed boolean telemetry constructor #143

--- src/Telemetry.cpp
+++ src/Telemetry.cpp
@@ -8,6 +8,14 @@ Telemetry::Telemetry() :
     // Nothing to do
 }
 
+Telemetry::Telemetry(const char *key, bool val)	:
+    m_type(DataType::TYPE_BOOL),
+    m_key(key),
+    m_value()
+{	
+        m_value.boolean = val;	
+}
+
 Telemetry::Telemetry(const char *key, float value) :
     m_type(DataType::TYPE_REAL),
     m_key(key),
@@ -31,6 +39,10 @@ bool Telemetry::IsEmpty() const {
 bool Telemetry::SerializeKeyValue(const JsonVariant &jsonObj) const {
     if (m_key) {
         switch (m_type) {
+            case DataType::TYPE_BOOL:	
+                jsonObj[m_key] = m_value.boolean;	
+                return true;	
+                break;
             case DataType::TYPE_INT:
                 jsonObj[m_key] = m_value.integer;
                 return true;
@@ -51,6 +63,9 @@ bool Telemetry::SerializeKeyValue(const JsonVariant &jsonObj) const {
     }
 
     switch (m_type) {
+        case DataType::TYPE_BOOL:	
+            return jsonObj.set(m_value.boolean);	
+            break;
         case DataType::TYPE_INT:
             return jsonObj.set(m_value.integer);
             break;
