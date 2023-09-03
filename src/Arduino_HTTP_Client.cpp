0b9200ae6bd0ff08460b275f221be7292236faf5
MathewHDYT <48954742+MathewHDYT@users.noreply.github.com>
2023-09-03 18:45:13 +0200

    Fix typos

--- src/Arduino_HTTP_Client.cpp
+++ src/Arduino_HTTP_Client.cpp
@@ -14,25 +14,25 @@ void Arduino_HTTP_Client::set_keep_alive(const bool& keep_alive) {
 }
 
 int Arduino_HTTP_Client::connect(const char *host, const uint16_t& port) {
-    return m_client.connect(host, port);
+    return m_http_client.connect(host, port);
 }
 
 void Arduino_HTTP_Client::stop() {
-    m_client.stop();
+    m_http_client.stop();
 }
 
 int Arduino_HTTP_Client::post(const char *url_path, const char *content_type, const char *request_body) {
-    return m_client.post(url_path, content_type, request_body);
+    return m_http_client.post(url_path, content_type, request_body);
 }
 
 int Arduino_HTTP_Client::get_response_status_code() {
-    return m_client.responseStatusCode();
+    return m_http_client.responseStatusCode();
 }
 
 int Arduino_HTTP_Client::get(const char *url_path) {
-    return m_client.get(url_path);
+    return m_http_client.get(url_path);
 }
 
 String Arduino_HTTP_Client::get_response_body() {
-    return m_client.responseBody();
+    return m_http_client.responseBody();
 }
