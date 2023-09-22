173706ca1673226d80e152bdb8a37c8604d36320
MathewHDYT <48954742+MathewHDYT@users.noreply.github.com>
2023-09-22 08:26:41 +0200

    Use initializer list

--- src/ThingsBoard.h
+++ src/ThingsBoard.h
@@ -950,7 +950,7 @@ class ThingsBoardSized {
       }
 
       // Subscribes to changes of the firmware information
-      const std::vector<const char *> fw_shared_keys(FW_CHKS_KEY, FW_CHKS_ALGO_KEY, FW_SIZE_KEY, FW_TITLE_KEY, FW_VER_KEY);
+      const std::vector<const char *> fw_shared_keys{FW_CHKS_KEY, FW_CHKS_ALGO_KEY, FW_SIZE_KEY, FW_TITLE_KEY, FW_VER_KEY};
       const Shared_Attribute_Callback fw_update_callback(std::bind(&ThingsBoardSized::Firmware_Shared_Attribute_Received, this, std::placeholders::_1), fw_shared_keys.cbegin(), fw_shared_keys.cend());
       return Shared_Attributes_Subscribe(fw_update_callback);
     }
