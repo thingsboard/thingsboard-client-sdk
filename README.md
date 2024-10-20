04484a4987b5a2245f171d56247dd649f803a5e4
MathewHDYT <48954742+MathewHDYT@users.noreply.github.com>
2024-10-20 15:48:23 +0200

    Fix usage of if constexpr for non C++20 boards

--- README.md
+++ README.md
@@ -670,10 +670,6 @@ class CustomLogger {
   public:
     template<typename ...Args>
     static int printfln(char const * format, Args const &... args) {
-        // Check if variadic template contains any actual additional arguments that should be inserted into format message
-        if (constexpr sizeof...(Args) == 0U) {
-            return 0;
-        }
         return 0;
     }
 };
--- src/DefaultLogger.h
+++ src/DefaultLogger.h
@@ -22,16 +22,24 @@ class DefaultLogger {
     /// @return Either the written amount of characters or an error indicator (being a negative number) if one occured
     template<typename ...Args>
     static int printfln(char const * format, Args const &... args) {
-        // Check if variadic template contains any actual additional arguments that should be inserted into format message
-        if (constexpr sizeof...(Args) == 0U) {
+        // Check if variadic template contains any actual additional arguments that should be inserted into format message.
+        // If it does, use constexpr if statement for C++20 supported devices to optimize away the branch instruction at compile time
+        // or alternatively runtime for devices that do not support C++20
+#if THINGSBOARD_ENABLE_CXX20
+        if constexpr (sizeof...(Args) == 0U) {
+#else
+        if (sizeof...(Args) == 0U) {
+#endif // THINGSBOARD_ENABLE_CXX20
             return printf(LOG_MESSAGE_FORMAT, format);
         }
-        int const size = Helper::detectSize(format, args...);
-        char arguments[size] = {};
-        int const written_characters = snprintf(arguments, size, format, args...);
-        // Written characters is expected to be one less, because of the null termination character
-        bool const result = (written_characters == (size - 1));
-        return printf(LOG_MESSAGE_FORMAT, result ? arguments : FAILED_MESSAGE);
+        else {
+            int const size = Helper::detectSize(format, args...);
+            char arguments[size] = {};
+            int const written_characters = snprintf(arguments, size, format, args...);
+            // Written characters is expected to be one less, because of the null termination character
+            bool const result = (written_characters == (size - 1));
+            return printf(LOG_MESSAGE_FORMAT, result ? arguments : FAILED_MESSAGE);
+        }
     }
 };
 
